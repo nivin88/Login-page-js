88d59af1d27fc71981680cf049a9d3aa87ef57cf
nivin88 <nivinkamaraj@gmail.com>
2024-09-24 17:28:13 +0530

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
-body{
-button.getBody
-}
+// const form = document.getElementById('myform');
+// let userName = document.getElementById('username');
+// let password = document.getElementById('password'); 
+// let nameError = document.getElementById('nameerrormsg');
+// let passwordError = document.getElementById('passworderrormsg');
+
+// form.addEventListener('submit',(event) => {
+//   event.preventDefault();  
+
+//     if( userName.value ===  ""){
+//       nameError.innerHTML = 'Name is required*'; 
+//       nameError.style.color = '#e74646';
+//     };
+
+//     if(password.value === "") { 
+//       passwordError.innerHTML='Password is required*';
+//       passwordError.style.color = '#e74646';
+//     };
+
+//     if( userName.value !==  ""){
+//       nameError.innerHTML = '';
+//     };
+
+//     if( password.value !==  ""){
+//       passwordError.innerHTML = '';
+//     };
+
+// });
 
