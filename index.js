499ea751a42bd47c58d3551d7911826dee205056
nivin88 <nivinkamaraj@gmail.com>
2024-09-25 17:08:21 +0530

    first commit

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
-// const form = document.getElementById('myform');
-// let userName = document.getElementById('username');
-// let password = document.getElementById('password'); 
-// let nameError = document.getElementById('nameerrormsg');
-// let passwordError = document.getElementById('passworderrormsg');
+const form = document.getElementById('mymethodform');
+let userName = document.getElementById('username');
+let Email = document.getElementById('Email');
+let password = document.getElementById('password'); 
+let nameError = document.getElementById('nameerrormsg');
+let EmailError = document.getElementById('Emailerrormsg');
+let passwordError = document.getElementById('passworderrormsg');
 
-// form.addEventListener('submit',(event) => {
-//   event.preventDefault();  
+form.addEventListener('submit',(event) => {
+  event.preventDefault();
+ 
+  if( userName.value !==  ""){
+    nameError.innerHTML = '';
+  };
+  
+  if(Email.value !==  ""){
+    EmailError.innerHTML = '';
+  };
 
-//     if( userName.value ===  ""){
-//       nameError.innerHTML = 'Name is required*'; 
-//       nameError.style.color = '#e74646';
-//     };
+  if(password.value !== ""){
+    passwordError.innerHTML = '';
+  }
+  
+  if( userName.value ===  ""){
+    nameError.innerHTML = 'Name is required*'; 
+    nameError.style.color = '#2def0fdf';
+   };
+   
+   if(Email.value === ""){
+    EmailError . innerHTML='Email is required*';
+    EmailError.style.color = '#2def0fdf';
+    };
+    
+    if(password.value === "") { 
+    passwordError.innerHTML='Password is required*';
+    passwordError.style.color = '#2def0fdf';
+    };
 
-//     if(password.value === "") { 
-//       passwordError.innerHTML='Password is required*';
-//       passwordError.style.color = '#e74646';
-//     };
-
-//     if( userName.value !==  ""){
-//       nameError.innerHTML = '';
-//     };
-
-//     if( password.value !==  ""){
-//       passwordError.innerHTML = '';
-//     };
-
-// });
+});
 
