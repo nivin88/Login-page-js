fcae387dce7c8e53a351ba922b96466648954185
nivin88 <nivinkamaraj@gmail.com>
2024-09-30 17:13:14 +0530

    first commit

--- index.js
+++ index.js
@@ -6,29 +6,46 @@ let nameError = document.getElementById('nameerrormsg');
 let EmailError = document.getElementById('Emailerrormsg');
 let passwordError = document.getElementById('passworderrormsg');
 
+
+
+function mysubmit(event){
+  event.preventDefault();
+  let submitsuccess=[]
+  const userName=document.getElementById('username').value
+  const Email=document.getElementById('Email').value
+  const password=document.getElementById('password').value
+
+  let uservalue=[{userName,Email,password}];
+  submitsuccess.push(uservalue)
+  console.log(submitsuccess)
+}
+
+
+
 form.addEventListener('submit',(event) => {
   event.preventDefault();
- 
+  
+
   if( userName.value !==  ""){
     nameError.innerHTML = '';
-    console.log(userName.value)
+    // console.log(userName.value)
   };
   
   if( Email.value !==  ""){
     EmailError.innerHTML = '';
-    console.log(Email.value)
+    // console.log(Email.value)
   };
 
   if( password.value !== ""){
     passwordError.innerHTML = '';
-    console.log(password.value)
+    // console.log(password.value)
   }
   
   if( userName.value ===  ""){
     nameError.innerHTML = 'Name is required*'; 
     nameError.style.color = '#2def0fdf';
-   };
-   
+   };  
+  
    if( Email.value === ""){
     EmailError . innerHTML='Email is required*';
     EmailError.style.color = '#2def0fdf';
@@ -38,5 +55,11 @@ form.addEventListener('submit',(event) => {
     passwordError.innerHTML='Password is required*';
     passwordError.style.color = '#2def0fdf';
     };
-});
 
+
+
+    document.getElementById('username').value = "";
+    document.getElementById('Email').value = "";
+    document.getElementById('password').value = "";
+
+});
